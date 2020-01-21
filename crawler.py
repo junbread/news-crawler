03e17afcf2194f5293a27ef65257da79c2923a1e
JunBread <junbbang5@gmail.com>
2020-01-21 16:27:50 +0900

    Add usage

--- crawler.py
+++ crawler.py
@@ -2,9 +2,18 @@ import requests
 import json
 import time
 import sys
+import os
+
+# argument check
+if (len(sys.argv) != 3):
+    print("Usage: python crawler.py <article_size> <save_path>")
+    exit(1)
+
+article_size = sys.argv[1]
+save_path = sys.argv[2]
 
 # get article list
-url = 'https://www.bigkinds.or.kr/api/news/search.do'
+url = "https://www.bigkinds.or.kr/api/news/search.do"
 data = {
     "indexName": "news",
     "searchKey": "",
@@ -18,12 +27,12 @@ data = {
     "incidentCodes": [4, 12, 49, 50, 51, 52, 53, 13, 54, 55,
                       56, 57, 58],
     "startNo": 1,
-    "resultNumber": sys.argv[1]
+    "resultNumber": article_size
 }
 
 resp = requests.post(url, json.dumps(data))
-results = json.loads(resp.text).get('resultList')
-links = map(lambda r: r.get('NEWS_ID'), results)
+results = json.loads(resp.text).get("resultList")
+links = map(lambda r: r.get("NEWS_ID"), results)
 
 # get article contents and save
 url = "https://www.bigkinds.or.kr/news/detailView.do"
@@ -32,17 +41,17 @@ for link in links:
         try:
             return requests.get(url, {"docId": link})
         except requests.ConnectionError as e:
-            time.sleep(1)
+            time.sleep(2)
             return try_request(link)
 
     resp = try_request(link)
     raw_article = json.loads(resp.text)
 
     print("id: {}, title: {}\n".format(
-        link, raw_article.get('detail').get('TITLE')))
+        link, raw_article.get("detail").get("TITLE")))
 
-    with open(link + ".story", "w", encoding="utf-8") as f:
+    with open(os.path.join(save_path, link + ".story"), "w", encoding="utf-8") as f:
         f.write("@title\n")
-        f.write(raw_article.get('detail').get('TITLE') + '\n')
+        f.write(raw_article.get("detail").get("TITLE") + "\n")
         f.write("@content\n")
-        f.write(raw_article.get('detail').get('CONTENT'))
+        f.write(raw_article.get("detail").get("CONTENT"))
